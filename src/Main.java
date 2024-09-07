d71b1ffc5d709a06bac3cd03b994731c81f92f67
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-09-07 13:54:13 +0300

    "Задача № 1"

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello mentor.Sergey will rise again!");
+
+        System.out.println("Задача № 1");
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        
+        }
+    }
+}
