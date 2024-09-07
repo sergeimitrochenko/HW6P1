064773590366bf3632e092d527f4748a74a14a08
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-09-07 14:45:42 +0300

    "Задача № 1 и 2 и 3"

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,9 @@ public class Main {
         for (int i = 10; i >= 1; i = i - 1) {
             System.out.println(i);
         }
+        System.out.println("Задача № 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
     }
 }
