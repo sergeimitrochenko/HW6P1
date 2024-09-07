0d6ef00b7f3a4e9d1567027e0a8b210eaf352b41
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-09-07 15:16:46 +0300

    "Задача № 1 и 2 и 3 и 4 и 5 и 6"

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,14 @@ public class Main {
         for (int i = 10; i >= -10; i = i - 1) {
             System.out.println(i);
         }
+        System.out.println("Задача № 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i);
+            System.out.println(i + " год является високосным");
+        }
+        System.out.println("Задача № 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
     }
 }
